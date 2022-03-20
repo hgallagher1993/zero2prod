bbc720afc10159f9a56752bd91df25709d3b286d
hgallagher1993 <hgallagher@protonmail.com>
2022-03-20 16:37:55 +0100

    Refactor and add first Integration test for health_check

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
 
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    run(listener)?.await
 }
