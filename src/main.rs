f418c47f03024e9c3838b2e811dadcdd552430af
hgallagher1993 <hgallagher@protonmail.com>
2022-03-20 12:37:04 +0100

    cargo + clippy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -8,11 +8,8 @@ async fn health_check() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
 
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
