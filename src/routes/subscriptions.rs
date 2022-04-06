8b44056e5846a9f463f834357c1222a9138c2549
hgallagher1993 <hgallagher@protonmail.com>
2022-04-06 17:40:02 +0100

    Run cargo fmt again

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,10 +1,7 @@
 use actix_web::{web, HttpResponse};
 
 #[derive(serde::Deserialize)]
-pub struct FormData {
-    /*email: String,
-    name: String,*/
-}
+pub struct FormData {}
 
 pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
